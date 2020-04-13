bdc397d869fd67c85aae6a1d6312a6d6c625a145
happou31 <happo31@gmail.com>
2020-04-14 01:37:51 +0900

    fix: 型定義

--- src/components/Static.tsx
+++ src/components/Static.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from "react";
 
 type Props = {
-  children?: React.ReactChild[];
+  children?: React.ReactChild | React.ReactChild[];
 };
 
 export default (props: Props) => {
