f05c557ecfdf26a47406aed465abf2c0c5e86909
happou31 <happo31@gmail.com>
2020-04-28 04:06:09 +0900

    add: メソッド分割

--- src/actions/Actions.ts
+++ src/actions/Actions.ts
@@ -1,4 +1,5 @@
 import VoiceData from "../model/GraphQLResult/VoiceData";
+import { voiceDataToVoiceList } from "../utils/GraphQLtoStore";
 
 export type Actions = ReturnType<
   typeof PlayAudioAction | typeof StopAudioAction | typeof FetchVoiceListAction
@@ -17,51 +18,8 @@ export const StopAudioAction = () => ({
 export const FETCH_VOICELIST = "FETCH_VOICELIST";
 
 export const FetchVoiceListAction = (voiceData: VoiceData) => {
-  const resultData = [];
-  const temp = [];
-  const match = /\d+_([^\/]+)\/([^\/]+)\/\d+_([^\.]+)\.mp3/;
-  for (const edge of voiceData.allFile.edges) {
-    const relativePath = edge.node.relativePath;
-    const matchResult = match.exec(relativePath);
-    if (matchResult) {
-      temp.push({
-        label: matchResult[1],
-        group: matchResult[2],
-        name: matchResult[3],
-        path: relativePath,
-      });
-    }
-  }
-
-  const findRowIndex = (label: string) => {
-    let i = 0;
-    for (const row of resultData) {
-      if (label === row.label) {
-        return i;
-      }
-      ++i;
-    }
-    return -1;
-  };
-  for (const audio of temp) {
-    let rowIndex = findRowIndex(audio.label);
-    if (rowIndex < 0) {
-      resultData.push({
-        label: audio.label,
-        audios: [],
-      });
-      rowIndex = resultData.length - 1;
-    }
-    const groupIndex = parseInt(audio.group);
-    while (resultData[rowIndex].audios.length <= groupIndex) {
-      resultData[rowIndex].audios.push([]);
-    }
-    resultData[rowIndex].audios[groupIndex].push({
-      path: audio.path,
-      label: audio.name,
-    });
-  }
-
+  const resultData = voiceDataToVoiceList(voiceData);
+  console.log({ voiceData, resultData });
   return {
     type: FETCH_VOICELIST as typeof FETCH_VOICELIST,
     voiceList: resultData,
---
+++ src/utils/GraphQLtoStore.ts
@@ -0,0 +1,50 @@
+import VoiceDataQueryResult from "../model/GraphQLResult/VoiceData";
+
+export function voiceDataToVoiceList(voiceData: VoiceDataQueryResult) {
+  const resultData = [];
+  const temp = [];
+  const match = /\d+_([^\/]+)\/([^\/]+)\/\d+_([^\.]+)\.mp3/;
+  for (const edge of voiceData.allFile.edges) {
+    const relativePath = edge.node.relativePath;
+    const matchResult = match.exec(relativePath);
+    if (matchResult) {
+      temp.push({
+        label: matchResult[1],
+        group: matchResult[2],
+        name: matchResult[3],
+        path: relativePath,
+      });
+    }
+  }
+
+  const findRowIndex = (label: string) => {
+    let i = 0;
+    for (const row of resultData) {
+      if (label === row.label) {
+        return i;
+      }
+      ++i;
+    }
+    return -1;
+  };
+  for (const audio of temp) {
+    let rowIndex = findRowIndex(audio.label);
+    if (rowIndex < 0) {
+      resultData.push({
+        label: audio.label,
+        audios: [],
+      });
+      rowIndex = resultData.length - 1;
+    }
+    const groupIndex = parseInt(audio.group);
+    while (resultData[rowIndex].audios.length <= groupIndex) {
+      resultData[rowIndex].audios.push([]);
+    }
+    resultData[rowIndex].audios[groupIndex].push({
+      path: audio.path,
+      label: audio.name,
+    });
+  }
+
+  return resultData;
+}
