5b8f8cb94decc40107cb429bd2a312948fab9072
happou31 <happo31@gmail.com>
2020-05-10 22:47:17 +0900

    change: favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 module.exports = {
   pathPrefix: "/marine_button",
   siteMetadata: {
@@ -26,7 +28,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-typescript`,
