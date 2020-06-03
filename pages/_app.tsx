2bbe8c8f8ea212165e1bd3a52a4fed11ee81d53c
happo31 <happo31@gmail.com>
2020-06-03 23:13:20 +0900

    fix: redux は _app でやるのがいいっぽい

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
+import appReducer from "../src/reducers/AppReducer";
+import playAudioReducer from "../src/reducers/PlayAudioReducer";
+
+const store = createStore(
+  combineReducers({ app: appReducer, playAudio: playAudioReducer })
+);
+
+export default ({
+  Component,
+  pageProps,
+}: {
+  // ここは真面目に型を付けるだけ無駄
+  Component: React.ComponentClass;
+  pageProps: Record<string, unknown>;
+}) => (
+  <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
+);
