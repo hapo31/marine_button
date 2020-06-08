98dfce1b6f2a1233b1029c8f76a1f4fd2fb9e184
happou31 <happo31@gmail.com>
2020-06-08 12:33:24 +0900

    add: PlayAudio class の定義と、置く場所を変えた

--- src/components/PlayAudio.tsx
+++
@@ -1,5 +0,0 @@
-// import React from "react";
-
-// type Props = {};
-
-// export default () => {};
--- src/state/PlayAudioState.ts
+++ src/state/PlayAudioState.ts
@@ -1,5 +1,52 @@
-export default class PlayAudioState {
-  filename: string;
+import fetch from "isomorphic-unfetch";
 
-  playing: boolean;
+export default class PlayAudioState {
+  filename = "";
+
+  playing = false;
+}
+
+export class PlayAudio {
+  private audioContext: AudioContext;
+
+  private arrayBuffers: Record<string, AudioBuffer> = {};
+
+  constructor() {
+    this.audioContext = new AudioContext();
+  }
+
+  public async load(url: string) {
+    // ロード済みなら即リターン
+    if (this.arrayBuffers[url] != null) {
+      return;
+    }
+    try {
+      const res = await fetch(url);
+      const buffer = await res.arrayBuffer();
+      const audioBuffer = await this.audioContext.decodeAudioData(buffer);
+      this.arrayBuffers[url] = audioBuffer;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  public async loadAndPlaySound(url: string, cache = true) {
+    // ロード済みでなければ読み込む
+    if (this.arrayBuffers[url] == null) {
+      try {
+        const res = await fetch(url);
+        const buffer = await res.arrayBuffer();
+        const audioBuffer = await this.audioContext.decodeAudioData(buffer);
+        if (cache) {
+          this.arrayBuffers[url] = audioBuffer;
+        }
+      } catch (e) {
+        throw e;
+      }
+    }
+
+    const src = this.audioContext.createBufferSource();
+    src.connect(this.audioContext.destination);
+    src.start(0);
+  }
 }
