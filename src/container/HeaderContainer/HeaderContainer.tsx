f668919afee57e4b5100cc6ae8c4bc727f5d5f39
happou31 <happo31@gmail.com>
2020-06-12 14:18:34 +0900

    fix: レスポンシブを若干真面目にやりました

--- src/container/HeaderContainer/HeaderContainer.tsx
+++ src/container/HeaderContainer/HeaderContainer.tsx
@@ -6,27 +6,29 @@ import ExternalLink from "../../components/ExternalLink";
 
 export default () => {
   return (
-    <header>
-      <ExternalLink
-        className="fork-me-on-github"
-        href="https://github.com/happou31/marine_button"
-      >
-        <img
-          width="120"
-          height="120"
-          src="https://github.blog/wp-content/uploads/2008/12/forkme_right_red_aa0000.png?resize=149%2C149"
-          className="attachment-full size-full"
-          alt="Fork me on GitHub"
-          data-recalc-dims="1"
-        />
-      </ExternalLink>
+    <>
+      <Nav className="navbar navbar-expand-lg nav-fill">
+        <ExternalLink
+          className="fork-me-on-github"
+          href="https://github.com/happou31/marine_button"
+        >
+          <img
+            width="120"
+            height="120"
+            src="https://github.blog/wp-content/uploads/2008/12/forkme_right_red_aa0000.png?resize=149%2C149"
+            className="attachment-full size-full"
+            alt="Fork me on GitHub"
+            data-recalc-dims="1"
+          />
+        </ExternalLink>
 
-      <Nav className="navbar navbar-expand-lg">
         <Navbar className="navbar-nav">
-          <NavItem className="mb-0 text-large wf-nicomoji">
+          <NavItem className="mb-0 header-contantainer-title text-large wf-nicomoji">
             <img alt="" className="site-logo" src="static/image/icon.png" />
             宝鐘マリンボタン
           </NavItem>
+        </Navbar>
+        <Navbar className="navbar-nav">
           <NavItem className="social-icon-container">
             <ExternalLink
               className="social-icon-youtube"
@@ -102,6 +104,6 @@ export default () => {
           </div>
         </Notice>
       </div>
-    </header>
+    </>
   );
 };
