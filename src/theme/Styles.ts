533632ab3447cba729ebd8a3beec986bfb238841
happou31 <happo31@gmail.com>
2020-06-24 03:48:55 +0900

    fix: ヘッダのアイコンズレがきもいので直した

--- src/theme/Styles.ts
+++ src/theme/Styles.ts
@@ -1,12 +1,79 @@
 import { makeStyles } from "@material-ui/core/styles";
+import zIndex from "@material-ui/core/styles/zIndex";
 
 const volumeContorollerHeight = "64px";
 
+const mainColor = "rgb(160, 45, 58)";
+const primaryColor = "rgb(192, 98, 102)";
+const subColor = "rgb(164, 109, 79)";
+
+const textSize = {
+  textLarge: {
+    fontSize: "52px",
+  },
+  textMid: {
+    fontSize: "28px",
+  },
+  textSmall: {
+    fontSize: "16px",
+  },
+  textVerySmall: {
+    fontSize: "10px",
+  },
+};
+
 const useStyles = makeStyles({
+  ...textSize,
+
+  nicomoji: {
+    display: "inline",
+    fontFamily: "Nico Moji Plus",
+  },
+
   header: {
     fontFamily: `"Nico Moji Plus", "Nico Moji", "Noto Sans JP"`,
-    backgroundColor: "var(--marine-sub-color)",
+    color: "black",
+    margin: "5px",
   },
+
+  headerArticle: {
+    marginBottom: "10px",
+    verticalAlign: "center",
+  },
+  headerContainer: {
+    alignContent: "center",
+    height: "80px",
+  },
+
+  headerItem: {
+    display: "flex-box",
+    verticalAlign: "top",
+    justifyContent: "center",
+    height: "52px",
+    lineHeight: "52px",
+  },
+
+  siteLogo: {
+    verticalAlign: "top",
+  },
+
+  headerSocialIconYoutube: {
+    objectFit: "contain",
+    display: "inline-block",
+    height: "32px",
+    marginTop: "12px",
+  },
+
+  headerSocialIconTwitter: {
+    objectFit: "contain",
+    display: "inline-block",
+    width: "100%",
+    height: "130%",
+    marginTop: "-2px",
+    marginLeft: "-50px",
+    zIndex: 1,
+  },
+
   footer: {
     fontFamily: `"Nico Moji Plus", "Nico Moji", "Noto Sans JP"`,
     backgroundColor: "var(--marine-sub-color)",
@@ -14,7 +81,6 @@ const useStyles = makeStyles({
   },
   volumeContoroller: {
     height: volumeContorollerHeight,
-    margin: "auto",
     borderColor: "black",
     top: "initial",
     bottom: 0,
