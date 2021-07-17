b534205121157467de5a31a1164d63fa5b7d8fba
happou31 <happo31@gmail.com>
2021-07-18 00:12:36 +0900

    fix: 古すぎるのでアップグレード

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 /* eslint-disable no-undef */
-module.exports = {};
+module.exports = (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+  };
+  return nextConfig;
+};
 /* eslint-enable */
