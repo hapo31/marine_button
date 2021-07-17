231dafd54f3b3a04a44b8fa870706830bf1637c6
happou31 <happo31@gmail.com>
2021-07-18 00:30:28 +0900

    fix: local 開発時は assetPrefix を空にする

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-undef */
+const env = process.env["NODE_ENV"];
 module.exports = (phase, { defaultConfig }) => {
   /**
    * @type {import('next').NextConfig}
    */
   const nextConfig = {
-    assetPrefix: "/marine_button",
+    assetPrefix: env === "production" ? "/marine_button" : "",
   };
   return nextConfig;
 };
