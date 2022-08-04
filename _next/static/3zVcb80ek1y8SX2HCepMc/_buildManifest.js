eb9c7a17b443111227f4bff3aff453a2baed18c9
hapo31 <hapo31@users.noreply.github.com>
2022-08-04 14:54:26 +0000

    deploy: 3758a25b460f397e59cdaebafcdf1e930e8fea4a

---
+++ _next/static/3zVcb80ek1y8SX2HCepMc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/331-23bb994a299edd89.js","static/chunks/pages/index-6ef2cac55f2ce403.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/FnQd8oq8L_vtJxOVOq4q_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/331-23bb994a299edd89.js","static/chunks/pages/index-b6e138f9a75bbc9d.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
